8bb93d378e9446e2d0136124ee360be962573068
14roebl <32593798+14roebl@users.noreply.github.com>
2019-02-12 18:58:09 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 18:55:55 2019
+
+@author: David Röbl
+"""
+
+from setuptools import setup
+
+setup(name='ges',
+      version='0.1',
+      description='geographic enrichment service',
+      url='https://github.com/14roebl/ges.git',
+      author='David Röbl',
+      author_email='david.roebl@yahoo.com',
+      license='MIT',
+      packages=['ges'],
+      zip_safe=False)
